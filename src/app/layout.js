cc9ebce0e916afca9a4bfbe7c672e98cc85bc792
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-11-07 16:08:57 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Oswald } from "next/font/google";
 import "./globals.css";
 import Navbar from "./(components)/Navbar";
 import Footer from "./(components)/Footer";
-
+import { Analytics } from '@vercel/analytics/react';
 const inter = Oswald({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Analytics/>
         <Footer/>
       </body>
     </html>
